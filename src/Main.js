63938286576130025fa2e7f09b8bbdcae2d72d45
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-28 14:17:02 +0300

    commit

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,48 @@
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 
 const Main = () => {
+    const [count, setCount] = useState(0);
+
+
+    let incrementCountFive = () => {
+        setCount(count + 5);
+      };
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let reset = () => {
+    setCount(0);
+  };
+
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  let decrementCountFive = () => {
+    setCount(count - 5);
+  };
+
+  
+
+
+
+
+
     return (
-        <div className = "circle">
-            <div className = "current-sum">0</div>
-            
+        <div>
+            <div className="circle">
+                <div className = "current-sum">{count}</div>
+            </div>
+
+            <button type="button" className="button" onClick={incrementCountFive}>Add five</button>
+            <button type="button" className="button" onClick={incrementCount}>Add one</button>
+            <button type="button" className="button" onClick={reset}>Reset</button>
+            <button type="button" className="button" onClick={decrementCount}>Remove one</button>
+            <button type="button" className="button" onClick={decrementCountFive}>Remove five</button>
+
         </div>
 
         
