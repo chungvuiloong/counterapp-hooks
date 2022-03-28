0c1b402d40a49e1582bfb5382f1b4b60f2b22901
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-28 12:36:12 +0300

    commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react.'
+
+
+export default const Header = () => {
+    return (
+        <div className="header">
+            <h1>Header</h1>
+        </div>
+    );
+};
